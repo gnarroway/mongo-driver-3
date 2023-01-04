d6a68c97dc1e4c55518ac4048b99df926710d62a
Krzysztof Władyka <krzysztof@wladyka.eu>
2023-01-04 17:24:14 +0100

    [DEV-12317] add github maven repo access

--- project.clj
+++ project.clj
@@ -1,14 +1,8 @@
 (defproject mongo-driver-3 "0.7.0"
-  :description "A Clojure wrapper for the Java MongoDB driver 3.11/4.0+."
-  :url "https://github.com/gnarroway/mongo-driver-3"
-  :license {:name         "The MIT License"
-            :url          "http://opensource.org/licenses/mit-license.php"
-            :distribution :repo}
-  :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
-                                    :username      :env/clojars_user
-                                    :password      :env/clojars_pass
-                                    :sign-releases false}]]
   :plugins [[lein-cljfmt "0.6.4"]]
-
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]
-                                  [org.mongodb/mongodb-driver-sync "4.7.1"]]}})
+                                  [org.mongodb/mongodb-driver-sync "4.7.1"]]}}
+  :repositories [["github" {:url "https://maven.pkg.github.com/getaroom/mongo-driver-3"
+                            :username "getaroom"
+                            :password :env/GITHUB_TOKEN
+                            :sign-releases false}]])
